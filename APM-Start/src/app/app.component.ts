c1657d14c604e6de0581d0cd028c7bc57bae919d
Sean Thornber <sean.thornber@accenture.com>
2023-03-20 12:32:37 +0000

    first-commit

---
+++ APM-Start/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>
+        Welcome to {{pageTitle}}!!
+      </h1>
+      <pm-products></pm-products>
+    </div>
+
+  `
+})
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
+}
